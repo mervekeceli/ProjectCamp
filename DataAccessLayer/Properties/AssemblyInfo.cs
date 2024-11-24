9ab768e9476aca409d9d26d0e6a9e714313f9f28
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-24 20:54:14 +0300

    Add project files.

---
+++ DataAccessLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94273f15-61bf-4344-be4f-50d6a2b078ae")]
