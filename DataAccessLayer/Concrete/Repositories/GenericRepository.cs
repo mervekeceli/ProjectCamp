8628e8e07fae4aa3c117c0e534191742007657c3
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-25 02:59:40 +0300

    Add Generic Repository

---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepositoryDal<T> where T : class
+    {
+        Context context = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>();
+        }
+        public void Delete(T item)
+        {
+            _object.Remove(item);
+            context.SaveChanges();
+        }
+
+        public void Insert(T item)
+        {
+            _object.Add(item);
+            context.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T item)
+        {
+            context.SaveChanges();
+        }
+    }
+}
