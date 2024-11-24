88cf2db4ce40ef0efe4e07cde80e8e146f8b8354
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-25 02:53:05 +0300

    Add Interfate & Repositories

--- DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +29,11 @@ namespace DataAccessLayer.Concrete.Repositories
         public List<Category> List()
         {
             return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(Category category)
