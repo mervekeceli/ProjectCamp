4743b583efb0b830ed6e442b4d870c943bd63b15
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-25 18:45:43 +0300

    Add category validator

---
+++ BusinessLayer/ValidationRules(FluentValidation)/CategoryValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules_FluentValidation_
+{
+    public class CategoryValidatior : AbstractValidator<Category>
+    {
+        public CategoryValidatior() 
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş olamaz!");
+            RuleFor(x=>x.CategoryDescription).NotEmpty().WithMessage("Açıklama boş olamaz!");
+            RuleFor(x => x.CategoryName).Length(50).WithMessage("50 karakterden fazla değer girişi yapamazsınız!");
+        }
+    }
+}
