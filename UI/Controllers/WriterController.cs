6cd05afe671cfd6de6e1090560e3f62a2180d693
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-26 22:07:58 +0300

    Writer Listed + Theme redundancies have been removed

---
+++ UI/Controllers/WriterController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EFWriterDal());
+        public IActionResult Index()
+        {
+            var writerValues = writerManager.GetList();
+            return View(writerValues);
+        }
+    }
+}
