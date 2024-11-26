7f6b111aea9e22878af45f3ddf4d1e1908fd46cc
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-26 19:24:05 +0300

    admin theme add category + category list added

--- UI/Controllers/AdminCategoryController.cs
+++ UI/Controllers/AdminCategoryController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation_;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public IActionResult Index()
         {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator validationRules = new CategoryValidator();
+            ValidationResult results = validationRules.Validate(category);
+
+            if (results.IsValid)
+            {
+               categoryManager.CagetgoryAdd(category);
+               return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var error in results.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+            }
             return View();
         }
     }
