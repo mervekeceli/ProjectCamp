361a3c13da5fa09d5f61f5ec56e2e11551d679ae
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-29 20:57:27 +0300

    Add Content controller

---
+++ UI/Controllers/AdminContentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class AdminContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContentByHeading() //Başlıkları içeriğe göre getir
+        {
+            return View();
+        }
+    }
+}
