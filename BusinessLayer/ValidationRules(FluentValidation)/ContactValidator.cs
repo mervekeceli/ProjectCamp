045ad00a3b3cc09b568009892b62f594238f1b9a
mervekeceli <mervekeceli55e5@gmail.com>
2024-11-30 04:04:00 +0300

    Inbox list actions

---
+++ BusinessLayer/ValidationRules(FluentValidation)/ContactValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules_FluentValidation_
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserEmail).NotEmpty().WithMessage("Mail adresini boş geçemezsiniz!");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu adını boş geçemezsiniz!");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kulanıcı adını boş geçemezsiniz!");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız!");
+            RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız!");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("100 karakterden fazla değer girişi yapamazsınız!");
+        }
+    }
+}
