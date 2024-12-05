46798f2ee358d69e05d32b870738a04f2cc265e3
mervekeceli <mervekeceli55e5@gmail.com>
2024-12-05 10:54:59 +0300

    Add Message class entity layer

--- UI/Controllers/AdminContactController.cs
+++ UI/Controllers/AdminContactController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation_;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
 {
     public class AdminContactController : Controller
     {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        ContactValidator validationRules = new ContactValidator();
         public IActionResult Index()
         {
-            return View();
+            var contactValues = contactManager.GetList();
+            return View(contactValues);
+        }
+
+        [HttpGet]
+        public IActionResult GetContactDetails(int id)
+        {
+            var contactValues = contactManager.GetById(id);
+            return View(contactValues);
+        }
+
+        public PartialViewResult ContactInboxPartial()
+        {
+            return PartialView();
         }
     }
 }
