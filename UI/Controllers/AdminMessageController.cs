9c6962b2f44456f9ed1ba9788c97072ae09d726a
mervekeceli <mervekeceli55e5@gmail.com>
2024-12-06 04:48:01 +0300

    Messages from message controller and admin

---
+++ UI/Controllers/AdminMessageController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IActionResult Inbox()
+        {
+            var messageList = messageManager.GetList();
+            return View(messageList);
+        }
+    }
+}
