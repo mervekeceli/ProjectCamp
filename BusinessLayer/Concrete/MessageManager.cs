c1488c05adc4336b2b5ba43571bc5e976cbb4829
mervekeceli <mervekeceli55e5@gmail.com>
2024-12-18 15:21:40 +0300

    Writer message operations

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -25,12 +25,12 @@ namespace BusinessLayer.Concrete
 
         public List<Message> GetListInbox()
         {
-            return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com");
+            return _messageDal.List(x => x.ReceiverMail == "mervekeceli@gmail.com");
         }
 
         public List<Message> GetListSendbox()
         {
-            return _messageDal.List(x => x.SenderMail == "admin@gmail.com");
+            return _messageDal.List(x => x.SenderMail == "mervekeceli@gmail.com");
         }
 
         public void MessageAdd(Message message)
---
+++ UI/Controllers/WriterPanel/WriterPanelMessageController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation_;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers.WriterPanel
+{
+    public class WriterPanelMessageController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
+        public IActionResult Inbox()
+        {
+            var messageList = messageManager.GetListInbox();
+            return View(messageList);
+        }
+
+        public IActionResult Sendbox()
+        {
+            var messageList = messageManager.GetListSendbox();
+            return View(messageList);
+        }
+
+        public PartialViewResult MessageListMenu()
+        {
+            return PartialView();
+        }
+    }
+}
