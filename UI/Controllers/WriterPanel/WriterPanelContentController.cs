ef10346f205fab202bb3c28ca215ecd83eb45bb4
mervekeceli <mervekeceli55e5@gmail.com>
2024-12-18 16:16:04 +0300

    Writer write writing :)

---
+++ UI/Controllers/WriterPanel/WriterPanelContentController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers.WriterPanel
+{
+    public class WriterPanelContentController : Controller
+    {
+        ContentManager contentManager = new ContentManager(new EfContentDal());
+        public IActionResult MyContent()
+        {
+            var contentValues = contentManager.GetListByWriter();
+            return View(contentValues);
+        }
+    }
+}
