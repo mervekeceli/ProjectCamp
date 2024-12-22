94aee8a227dc1e0344f527f81cd0f9b6e43e544c
mervekeceli <mervekeceli55e5@gmail.com>
2024-12-22 05:12:39 +0300

    showpage operations continued

--- UI/Controllers/DefaultController.cs
+++ UI/Controllers/DefaultController.cs
@@ -7,16 +7,20 @@ namespace UI.Controllers
     public class DefaultController : Controller
     {
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        ContentManager contentManager = new ContentManager(new EfContentDal());
         //GET : Default
-        public IActionResult Index()
+        public PartialViewResult Index()
         {
-            return View();
+            var contentList = contentManager.GetList();
+            var headingList = headingManager.GetList();
+            return PartialView(Tuple.Create(headingList, contentList));
         }
 
         public IActionResult Headings()
         {
+            var contentList = contentManager.GetList();
             var headingList = headingManager.GetList();
-            return View(headingList);
+            return View(Tuple.Create(headingList, contentList));
         }
     }
 }
