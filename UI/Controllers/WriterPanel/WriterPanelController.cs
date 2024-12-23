c3e91cfc35d9c59e2ac773eb200a9dd8839eaa2c
mervekeceli <mervekeceli55e5@gmail.com>
2024-12-24 02:29:22 +0300

    WriterPorfile paged done

--- UI/Controllers/WriterPanel/WriterPanelController.cs
+++ UI/Controllers/WriterPanel/WriterPanelController.cs
@@ -1,10 +1,13 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation_;
+using ValidationResult = FluentValidation.Results.ValidationResult;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using X.PagedList.Extensions;
 
@@ -14,10 +17,40 @@ namespace UI.Controllers.WriterPanel
     {
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
         CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+
+        WriterValidator writerValidator = new WriterValidator();
 
         Context context = new Context();
-        public IActionResult WriterProfile()
+
+        [HttpGet]
+        public IActionResult WriterProfile(int id)
         {
+            string writerMailInfo = User.FindFirstValue(ClaimTypes.Email);
+            id = context.Writers.Where(x => x.WriterEmail == writerMailInfo).Select(x => x.WriterId).FirstOrDefault();
+            var writerValue = writerManager.GetById(id);
+
+            return View(writerValue);
+        }
+
+
+        [HttpPost]
+        public IActionResult WriterProfile(Writer writer)
+        {
+            ValidationResult results = writerValidator.Validate(writer);
+
+            if (results.IsValid)
+            {
+                writerManager.WriterUpdate(writer);
+                return RedirectToAction("AllHeading", "WriterPanel");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
 
